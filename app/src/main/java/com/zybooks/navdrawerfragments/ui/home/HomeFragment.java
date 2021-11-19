a84efda39f2a7eb8172149f8a91bb4b055ea4d9e
isaakwheeler <77463529+isaakwheeler@users.noreply.github.com>
2021-11-19 13:38:19 -0800

    working on HomeFragment.java

--- app/src/main/java/com/zybooks/navdrawerfragments/ui/home/HomeFragment.java
+++ app/src/main/java/com/zybooks/navdrawerfragments/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.zybooks.navdrawerfragments.ui.home;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -21,6 +23,22 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home,container,false);
         //TODO: Sets the onClickListener for each button to the galleryItemButtonClick method with appropriate arguments
+
+
+        Button mGalleryItem1 = root.findViewById(R.id.GalleryItem1);
+        Button mGalleryItem2 = root.findViewById(R.id.GalleryItem2);
+        Button mGalleryItem3 = root.findViewById(R.id.GalleryItem3);
+
+
+        mGalleryItem1.setOnClickListener((View v) -> {
+            galleryItemButtonClick(v, );
+        });
+        mGalleryItem2.setOnClickListener((View v) -> {
+            // do something here
+        });
+        mGalleryItem3.setOnClickListener((View v) -> {
+            // do something here
+        });
 
         return root;
     }
